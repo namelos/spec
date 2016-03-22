170d920910113878ea1972b30695dfa9c794aa4b
Fred Wang <namelos@outlook.com>
2016-03-22 19:22:46 +0800

    add . to travis

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   it 'is valid with a firstname, lastname and email'
   it 'is invalid without a firstname'
   it 'is invalid without a lastname'

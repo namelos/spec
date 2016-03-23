3c765c41aa774521cb36c375d7005861e18ca5ca
Fred Wang <namelos@outlook.com>
2016-03-23 19:25:24 +0800

    add factory

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,10 +1,26 @@
 FactoryGirl.define do
   factory :contact do
-    firstname "MyString"
-    string "MyString"
-    lastname "MyString"
-    string "MyString"
-    email "MyString"
-    string "MyString"
+    firstname { Faker:: Name.first_name }
+    lastname { Faker:: Name.last_name }
+    email { Faker:: Internet.email }
+
+    after(:build) do |contact|
+      [:home_phone, :work_phone, :mobile_phone].each do |phone|
+        contact.phones << build(:phone,
+                                phone_type: phone,
+                                contact: contact)
+      end
+    end
+
+    # firstname 'John'
+    # lastname 'Doe'
+    # sequence(:email) { |n| "johndoe#{n}@example.com" }
+
+    # firstname "MyString"
+    # string "MyString"
+    # lastname "MyString"
+    # string "MyString"
+    # email "MyString"
+    # string "MyString"
   end
 end
--- spec/factories/phones.rb
+++ spec/factories/phones.rb
@@ -1,7 +1,23 @@
 FactoryGirl.define do
   factory :phone do
-    phone_type "MyString"
-    phone "MyString"
-    contact nil
+    association :contact
+    # phone '123-555-1234'
+    phone { Faker::PhoneNumber.phone_number }
+
+    factory :home_phone do
+      phone_type 'home'
+    end
+
+    factory :work_phone do
+      phone_type 'work'
+    end
+
+    factory :mobile_phone do
+      phone_type 'mobile'
+    end
+
+    # phone_type "MyString"
+    # phone "MyString"
+    # contact nil
   end
 end

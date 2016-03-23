f2349bb1670b4b1cf9efe15dbfd5005d6c46d264
Fred Wang <namelos@outlook.com>
2016-03-23 10:43:01 +0800

    add contact spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,10 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  it 'is valid with a firstname, lastname and email'
-  it 'is invalid without a firstname'
-  it 'is invalid without a lastname'
-  it 'is invalid without an email address'
-  it 'is invalid with a duplicate email address'
+  it 'is valid with a firstname, lastname and email' do
+    contact = Contact.new(firstname: 'Aaron',
+                          lastname: 'Sumner',
+                          email: 'tester@example.com')
+    expect(contact).to be_valid
+  end
+
+  it 'is invalid without a firstname' do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to include("can't be blank")
+  end
+
+  it 'is invalid without a lastname' do
+    contact = Contact.new(lastname: nil)
+    contact.valid?
+    expect(contact.errors[:lastname]).to include("can't be blank")
+  end
+
+  it 'is invalid without an email address' do
+    contact = Contact.new(email: nil)
+    contact.valid?
+    expect(contact.errors[:email]).to include("can't be blank")
+  end
+
+  it 'is invalid with a duplicate email address' do
+    Contact.create(firstname: 'Joe',
+                   lastname: 'Tester',
+                   email: 'tester@example.com')
+
+    contact = Contact.new(firstname: 'Jane',
+                          lastname: 'Tester',
+                          email: 'tester@example.com')
+    contact.valid?
+    expect(contact.errors[:email]).to include('has already been taken')
+  end
+
   it "returns a contact's full name as a string"
+
 end
